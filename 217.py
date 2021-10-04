d3c5ab9f4c0a669d150f7e074b5f3fd0e38e621d
dong <chosd1123@naver.com>
2021-10-04 12:16:13 +0900

    정답

--- 217.py
+++ 217.py
@@ -1,24 +1,18 @@
-D = [0]*100
+x = int(input())
+d = [0]*10000
 
-def DP(x):
-    c = 0
+for i in range(2, x+1):
+    d[i] = d[i-1]-1
+    if d[i]%5==0 :
+        d[i] = min(d[i], (d[i]//5)+1)
+    if d[i]%2==0 :
+        d[i] = min(d[i], (d[i]//2)+1)
+    if d[i]%3==0 :
+        d[i] = min(d[i], (d[i]//2)+1)
 
-
-    if x//5 ==0 :
-        return D[x]
-        c += 1 
-    elif x//3 == 0 :
-        return D[x]
-        c += 1
-    elif x//2 == 0 :
-        return D[x]
-        c += 1
-    else :
-        x -= 1
-        c += 1
+print(d[x])
     
-    D[x] = DP[x]
-    return c
+    
 
 
 
