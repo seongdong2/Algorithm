9eca7a7ba6d595f8df9b47f1e2dcbba94eace5cb
dong <chosd1123@naver.com>
2021-10-12 08:34:51 +0900

    '곱하기혹은더하기'

--- 그리디 문제/곱하기 혹은 더하기.py
+++ 그리디 문제/곱하기 혹은 더하기.py
@@ -2,7 +2,7 @@ s = list(map(int, input()))
 rst = 0
 
 for i in range(len(s)) :
-    if s[i-1] == 0 or s[i] == 0 or s[i] == 1 :
+    if s[i] == 0 or rst == 0:
         rst += s[i]
     else :
         rst *= s[i]
