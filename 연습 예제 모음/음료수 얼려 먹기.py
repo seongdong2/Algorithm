732570cdb968f7c0a707b9ede50a00b0eb3937ca
dong <chosd1123@naver.com>
2021-10-15 08:36:14 +0900

    책 정답

--- 연습 예제 모음/음료수 얼려 먹기.py
+++ 연습 예제 모음/음료수 얼려 먹기.py
@@ -1,18 +1,32 @@
 #묶음을 찾아주는 프로그램은 DFS를 이용하면 간단하게 해결할 수 있다.
-from collections import deque
+n, m = map(int, input().split())
 
-def bfs(gragh, a, b, state) :
-    q=deque([a][b])
-    state[a][b] = 1
+gragh = []
+for _ in range(n) :
+    gragh.append(list(map(int, input())))
 
-    while q : 
-        for i in gragh[a] :
-            if not state[a][i] != 1 :
-                now = q.popleft()
-                state[a][i] == 1
-            q.append(q[a][i])
-    return bfs(gragh, now[0], now[1], state)
-        
+
+def dfs(x, y) :
+    if x <= -1 or x >= n or y <= -1 or y >= m :
+        return False
+
+#0인 지점 하나를 찾아서 좌표 별로 T/F 부여
+#재귀를 통해 기준점부터 깊이 탐색
+    if gragh[x][y] == 0 :
+        gragh[x][y] == 1
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+        return True
+    return False
+
+rst = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True :
+            rst +=1
+
 
                 
 
