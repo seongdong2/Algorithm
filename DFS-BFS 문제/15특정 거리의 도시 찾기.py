5f01e59b964fc7114c7a71bdd62295cb2e1835c0
dong <chosd1123@naver.com>
2021-10-14 22:08:38 +0900

    211014

--- DFS-BFS 문제/15특정 거리의 도시 찾기.py
+++ DFS-BFS 문제/15특정 거리의 도시 찾기.py
@@ -1,22 +1,39 @@
-def dfs(array, x, visited):
-    if 
-    visited[x] = True
-    for i in array[x]:
-        if not visited[i]:
-            dfs(array, i, visited)
-        
+#'모든 도로의 거리가 1'이라는 조건 덕분에 너비 우선 탐색을 이용하여 간단히 해결할 수 있다.
 
-
-
+from collections import deque
 
 n, m, k, x = map(int, input().split())
-visited = [False]*n
+gragh = [[] for _ in range(n+1)]
 
-array = []
-for _ in m :
-    array.append(map(int, input().split()))
+for _ in range(m):
+    a, b = map(int, input().split())
+    gragh[a].append(b)
 
-dfs(array, x, visited)
+#모든 도시에 대한 최단 거리 초기화
+
+distance = [-1] * (n+1)
+distance[x] = 0
+
+q = deque([x])
+
+while q :
+    now = q.popleft()
+    for next_node in gragh[now] :
+        if distance[next_node] == -1 :
+            #최단 거리 갱신
+            distance[next_node] == distance[now] + 1
+            q.append(next_node)
+
+check = False
+for i in range(1, n+1):
+    if distance[i] == k :
+        print(i)
+        check = True
+
+if check == False :
+    print(-1)
+
+
 
 
 
