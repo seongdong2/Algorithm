1dcaf977c9cbe0b5e1e740818a799980ecb3c909
dong <chosd1123@naver.com>
2021-10-21 23:22:03 +0900

    백준

--- 백준문제/터렛.py
+++ 백준문제/터렛.py
@@ -9,15 +9,18 @@ def dis(a,b,c,d):
     return rst
 
 def case(d, r1, r2):
-    if d == r1+r2:
-        return 1
-    elif d > r1+r2:
+    if r1> d or r2 > d:
         return 0
-    elif d < r1+r2 :
-        return 2
-    else :
-        return -1
-    
+    else : 
+        if d == r1+r2:
+            return 1
+        elif d > r1+r2:
+            return 0
+        elif d < r1+r2 :
+            return 2
+        else :
+            return -1
+        
 distance = dis(x_1, y_1, x_2, y_2)
 nice = case(distance, r_1, r_2)
 print(nice)
