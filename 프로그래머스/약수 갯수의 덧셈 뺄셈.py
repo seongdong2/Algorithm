42423dbac8ac2316a72163fc6a665f3c1e98593c
dong <chosd1123@naver.com>
2021-10-29 11:19:29 +0900

    더 나은 풀이

--- 프로그래머스/약수 갯수의 덧셈 뺄셈.py
+++ 프로그래머스/약수 갯수의 덧셈 뺄셈.py
@@ -1,31 +1,11 @@
+#약수의 갯수가 홀수인 경우는 제곱수인 경우 뿐이다.
 def solution(left, right):
     answer = 0
-    
-    def count(x):
-        cnt=0
-        
-        if x == 1:
-            return 1
-        
-        
-        for i in range(1, ((x+1)//2)+1):
-            if x%i == 0:
-                cnt += 1
-        
-        if cnt % 2 != 0:
-            cnt -= 1
-            cnt *= 2
-            cnt += 1
-        else :
-            cnt *=2
-        
-        return cnt+2
-            
-        
-    for num in range(left, right+1):
-        if count(num) % 2 == 0:
-            answer -= num
-        else: 
-            answer += num
-    
+    for i in range(left, right+1):
+        if isEven(i) : answer +=1
+        else :answer -= 1
     return answer
+
+#1/2 제곱수가 정수면 > 홀수의 약수 갯수를 가지게 됌.
+def isEven(num):
+    return True if (num**(1/2))%1 > 0 else False
