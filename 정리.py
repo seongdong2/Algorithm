e55f5240ddeb72b99869f3a17ba86a9c12a855aa
dong <chosd1123@naver.com>
2021-11-16 13:36:57 +0900

    pull

--- 정리.py
+++ 정리.py
@@ -9,6 +9,8 @@ def dfs(gragh, start, visited):
             dfs(gragh, v, visited)
 
 
+
+
 #BFS, O(N)
 #DFS보다 빠른 탐색
 from collections import deque
@@ -131,15 +133,22 @@ def DP2(n):
     return d[n]
 
 
-
-#enumerate
-a= ['hong', 'gil', 'dong']
-b= []
-c= {}
-def enu(a, b, c):
-    for i, name in enumerate(a):
-        b.append((i,name))
-        c[i]=b[name]
+#아라토스테네스의 채
+import math
+def aratos(n):
+    array = [True for i in range(n+1)]
+    
+    for i in range(2, int(math.sqrt(n))+1):
+        if array[i] == True :
+            j=2
+            while i*j <=n:
+                array[i*j]=False
+                j += 1
+    
+    for i in range(2, n+1):
+        if array[i]:
+            print(i, end =' ')
 
 
  
+#pull test
