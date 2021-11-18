59384a35e77c2f8cb9f00447bcbbd8c4874a14ae
dong <chosd1123@naver.com>
2021-11-18 19:12:55 +0900

    ㅇ

--- dd.py
+++ dd.py
@@ -1,13 +1,1 @@
-a= ['hong', 'gil', 'dong']
-b= []
-c= {}
-def enu(a, b, c):
-    for i, name in enumerate(a):
-        b.append([i,name])
-        c[i]=b[i][1]
-
-enu(a,b,c)
-
-print(a)
-print(b)
-print(c)
+#원숭이도 이해할 수 있는 git 명령어
